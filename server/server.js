51d6ebf64fa8182b523a660fe351fc35c1524b63
Yuvraj Soni <yuvrajsoni2k1@gmail.com>
2025-07-18 00:35:25 +0530

    Initial commit with client and server

---
+++ server/server.js
@@ -0,0 +1,25 @@
+require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
+const connectDB = require('./config/db');
+const transcriptionRoutes = require('./routes/transcriptionRoutes');
+
+const app = express();
+
+// Connect to DB
+connectDB();
+
+// Middleware
+app.use(cors({
+  origin: 'http://localhost:3000',
+  methods: ['GET', 'POST'],
+  allowedHeaders: ['Content-Type'],
+}));
+app.use(express.json());
+
+// API Routes
+app.use('/api/transcriptions', transcriptionRoutes);
+
+// Start Server
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
